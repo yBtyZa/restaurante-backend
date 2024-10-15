f34408cf2cbcba997b8073f9a74ea228e0b259ab
Guilherme Betsa <ybtyza@gmail.com>
2024-10-14 22:53:47 -0300

    feat: 🎸 (Pratos) Criando função para cadastrar um prato

--- src/routes/restaurantes.router.js
+++ src/routes/restaurantes.router.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 const RestauranteController = require('../dominios/restaurantes/controllers/restaurante.controller');
+const PratosController = require('../dominios/restaurantes/controllers/pratos.controller');
+const validaToken = require('../middlewares/validaToken');
 
 const restauranteRouter = Router();
 
 restauranteRouter.post('/', RestauranteController.create);
 
 //Rotas Pratos
-restauranteRouter.post('/:id/pratos', RestauranteController.createPrato)
+restauranteRouter.post('/:id/pratos', validaToken, PratosController.create)
 
 module.exports = restauranteRouter;
