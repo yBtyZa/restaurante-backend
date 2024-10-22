82c8de2b7f26aad0c9912fc6945c979a0faf3041
Guilherme Betsa <ybtyza@gmail.com>
2024-10-22 02:30:49 -0300

    feat: 🎸 (Permissões) Validando permissões de usuario

--- src/routes/pagamentos.router.js
+++ src/routes/pagamentos.router.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
 const pagamentosController = require('../dominios/pagamentos/controllers/pagamentos.controller');
 
+const validaToken = require('../middlewares/validaToken');
+const controlePermissao = require('../middlewares/controlePermissao');
+
 const pagamentosRouter = Router();
 
-pagamentosRouter.get('/:id', pagamentosController.listOne);
+pagamentosRouter.get('/:id', validaToken, controlePermissao([ 'admin', 'restaurante', 'cliente' ]), pagamentosController.listOne);
 
 module.exports = pagamentosRouter;
